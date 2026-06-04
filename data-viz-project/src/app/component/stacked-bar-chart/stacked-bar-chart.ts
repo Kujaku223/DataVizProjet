ec530146f28a3735d9f24063f061010aa835f98c
Gabriel <daniuriza@outlook.com>
2026-06-04 18:09:36 -0400

    pushed example chart on stacked bar chart

--- data-viz-project/src/app/component/stacked-bar-chart/stacked-bar-chart.ts
+++ data-viz-project/src/app/component/stacked-bar-chart/stacked-bar-chart.ts
@@ -1,5 +1,6 @@
-import {Component, inject} from '@angular/core';
+import {afterNextRender, Component, ElementRef, inject, viewChild} from '@angular/core';
 import {DataManipulation} from '../../service/data-manipulation';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-stacked-bar-chart',
@@ -8,6 +9,75 @@ import {DataManipulation} from '../../service/data-manipulation';
   styleUrl: './stacked-bar-chart.scss',
 })
 export class StackedBarChart {
-  private dataManipulationService = inject(DataManipulation)
+ private dataManipulationService = inject(DataManipulation);
+  private chartContainer = viewChild<ElementRef>('chartContainer');
 
+  constructor() {
+    // Ensure D3 only manipulates the DOM on the browser
+    console.log("ccc");
+    afterNextRender(() => {
+      this.createChart();
+    });
+  }
+
+
+private createChart(): void {
+    // https://kkirtigoel01.medium.com/mastering-data-visualization-best-practices-with-d3-js-and-angular-3687531cb88f
+    const element = this.chartContainer()?.nativeElement;
+    if (!element)
+        return
+
+    const width = 500;
+    const height = 300;
+    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+
+    // Sample data
+    const data = [
+      { name: 'A', value: 30 },
+      { name: 'B', value: 80 },
+      { name: 'C', value: 45 },
+      { name: 'D', value: 60 }
+    ];
+
+    // Clear any existing SVGs
+    d3.select(element).selectAll('svg').remove();
+
+    // Create the SVG container
+    const svg = d3.select(element)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    // Create scales
+    const x = d3.scaleBand()
+      .domain(data.map(d => d.name))
+      .range([margin.left, width - margin.right])
+      .padding(0.1);
+
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.value) || 0])
+      .nice()
+      .range([height - margin.bottom, margin.top]);
+
+    // Draw the bars
+    svg.append('g')
+      .attr('fill', 'steelblue')
+      .selectAll('rect')
+      .data(data)
+      .join('rect')
+      .attr('x', d => x(d.name) || 0)
+      .attr('y', d => y(d.value))
+      .attr('height', d => y(0) - y(d.value))
+      .attr('width', x.bandwidth());
+
+    // Add X axis
+    svg.append('g')
+      .attr('transform', `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    svg.append('g')
+      .attr('transform', `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y));
+  }
 }
